81467029f480720a7b67dc44ce9bafcff4016709
Mithat Gülen <260512@student.glu.nl>
2026-09-15 11:10:16 +0200

    Update sketch.js
    If-Else statement Opdracht (De "Random()" functie werkt niet)

--- Oefenopdrachten/Opdracht 3/sketch.js
+++ Oefenopdrachten/Opdracht 3/sketch.js
@@ -1,7 +1,24 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 200);
 }
 
 function draw() {
   background(220);
+
+  let score = 90;
+  // random(score = score >= 1 || score <= 100) // [Deze code werkt niet, en ik weet niet hoe ik het can behandelen]
+
+  if (score >= 90) {
+    fill(0, 255, 0);
+    text("Uitstekent!", 20, 20);
+  } else if (score >= 70 && score <= 89 ) {
+    fill(255, 255, 0);
+    text("Goed gedaan!", 20, 40);
+  } else if (score >= 50 && score <= 69) {
+    fill(255, 125, 0);
+    text("Voldoende...", 20, 60);
+  } else {
+    fill(255, 0, 0);
+    text("Onvoldoende...", 20, 80);
+  }
 }

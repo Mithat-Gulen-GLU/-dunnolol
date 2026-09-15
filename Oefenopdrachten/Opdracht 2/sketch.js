d2ad672897e8e7b02f813f6cdeb2174116a7ec26
Mithat Gülen <260512@student.glu.nl>
2026-09-15 11:11:52 +0200

    Update sketch.js
    Oefenopdracht 2

--- Oefenopdrachten/Opdracht 2/sketch.js
+++ Oefenopdrachten/Opdracht 2/sketch.js
@@ -4,4 +4,35 @@ function setup() {
 
 function draw() {
   background(220);
+
+// Hier staat de Variabelen
+let x = 100;
+let a = 20;
+let b = 10;
+
+// Dit is het Greeting Variabel
+let greeting = "Hello World!";
+
+// Deze vier Variabelen zijn speciaal doordat wij hen later gaan defineren, ze hebben eigenlijk geen waardes om zijn eigen
+let optellen;
+let aftrekken;
+let vermeningvuldigen;
+let delen;
+
+// Hier defineren wij de vorige fier Variabelen door specifieke situaties met de twee variabelen "a" en "b" te gebruiken zoals optellen, aftrekken, etc.
+optellen = a + b;
+aftrekken = a - b;
+vermeningvuldigen = a * b;
+delen = a / b;
+
+text(x, 20, 20);
+
+// Hier tonen wij het Greeting Variabel
+text(greeting, 20, 60);
+
+// Hier tonen wij het resultaten van onze rekeningen
+text("Optellen:" + optellen, 20, 100);
+text("Aftrekken:" + aftrekken, 20, 120);
+text("Vermeningvuldigen:" + vermeningvuldigen, 20, 140);
+text("Delen:" + delen, 20, 160);
 }

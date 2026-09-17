96f3a8ba77fd2d8453674130f5c628d2d7ad2268
Mithat Gülen <260512@student.glu.nl>
2026-09-17 14:29:21 +0200

    Update sketch.js
    Oefenopdracht 5(?)
    
    Tried to learn "random();" did not work out in the end

--- Oefenopdrachten/Opdracht 5/sketch.js
+++ Oefenopdrachten/Opdracht 5/sketch.js
@@ -1,7 +1,22 @@
+let a = random(0, 100);
+let b = random(0, 100);
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
   background(220);
+  if (keyCode === 32) {
+    text("Waarde a:" + a, 20, 20);
+    text("Waarde b:" + b, 20, 40);  
+  }
+
+  // if (a > b) {
+  //   text("a is groter dan b");
+  // } else if (a < b) {
+  //   text("a is kleiner dan b");
+  // } else {
+  //   text("a en b zijn gelijk");
+  // }
 }

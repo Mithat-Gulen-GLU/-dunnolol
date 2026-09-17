26ed082611c006c73af9815a5dff81fb3b9b481a
Mithat Gülen <260512@student.glu.nl>
2026-09-17 14:27:16 +0200

    Update sketch.js
    Oefenopdract 4
    
    Subsection: Input
    Added button input code as well as mouse input code for practicing purposes

--- Oefenopdrachten/Opdracht 4/sketch.js
+++ Oefenopdrachten/Opdracht 4/sketch.js
@@ -1,7 +1,62 @@
+// let keyCode = GamepadButton(ENTER);
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1000, 450);
 }
 
 function draw() {
   background(220);
+  
+  // MOUSE POSITION TRACKER
+  // line(mouseX, 0, mouseX, 100,);
+
+  // MOUSE CLICK CHECK
+  // if (mouseIsPressed === true) {
+  //   console.log("Mouse Click");
+  // }
+
+  // KEYBOARD BUTTON SPECIFIC CLICK CHECK
+  // if (keyIsPressed === true) {
+  //   if (keyCode === ENTER) {
+  //     console.log("Anan");
+  //   }
+  // }
+
+  // NUMBER-BY-KEYBOARD-BUTTON INPUT
+  // if (keyCode === 32) {
+  //   console.log("Space Bar sells drinks");
+  // }
 }
+
+
+
+// KEYBOARD BUTTON PRESS INPUT
+
+// PRESS
+
+// function keyPressed() {
+//   if (keyCode === ENTER) {
+//     console.log("Werkt!");
+//   }
+// }
+
+
+
+// RELEASE
+
+// function keyReleased() {
+//   if (keyCode === ENTER) {
+//     console.log("Werkt niet meer!");
+//   }
+// }
+
+
+
+// MOUSE CLICK INPUT
+// function mousePressed() {
+//   if (mouseButton === LEFT) {
+//     console.log("Left CLick!");
+//   } else if (mouseButton === RIGHT) {
+//     console.log("Right Click!");
+//   }
+// }

1388b40f2e4500b27e8ef68c95a1f255a621b1e3
Mithat Gülen <260512@student.glu.nl>
2026-09-25 11:26:11 +0200

    Create sketch.js
    this is for my school assignment

---
+++ Oefenopdrachten/Block 2 Opdract 1/sketch.js
@@ -0,0 +1,17 @@
+let x = 200;
+
+function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background(230);
+  fill(120, 255, 0);
+  fill(255, 100, 0);
+  ellipse(x, 120, 160);
+
+  ellipse(x, 320, 160);
+
+  x = x + 1;
+  x = x + 1;
+}

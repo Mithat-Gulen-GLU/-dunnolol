273661e1842eb0b638b9e65def41296c5c21f2c3
Mithat Gülen <260512@student.glu.nl>
2026-09-25 20:44:30 +0200

    Update sketch.js
    Forward Preparations for Week 5 Opdracht

--- Week 4/sketch.js
+++ Week 4/sketch.js
@@ -1,7 +1,33 @@
+// VARIABLES
+// VARIABELEN MOETEN LOGISCHE NAMEN HEBBEN
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 600);
 }
 
 function draw() {
   background(220);
+
+  // PROSEDURAL GENERATION
+  // ARRAYS & LOOPS
+  // "draw();" OM ARRAYS TE LEZEN
+
+  // FORMS
+  // VERSCHILLENDE VORMEN ELKE KEER
+  // VARIATIES PER POSITIE, VORM, KLEUR EN AANTAL (MINIMAAL VIERKANTEN EN CIRCLES)
+
+  // COLORS
+
+  // NUMBERS
+
+  // POSITION
+
+  // BUG FIXING
 }
+
+// ACTIVATION BY BACKSPACE
+// function keyPressed () {
+//   if (keyCode === BACKSPACE) {
+//     z
+//   }
+// }
